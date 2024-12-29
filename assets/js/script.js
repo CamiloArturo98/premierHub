406d967df8405983e2bd69edbfcc238014221ef4
Camm <arthurxx198@gmail.com>
2024-12-28 23:37:35 -0500

    adding first files

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Cartelera de peliculas
+// document.querySelectorAll('#movies .movies-grid > div').forEach(movie => {
+//     const trailerBtn = movie.querySelector('button:first-of-type');
+//     const buyBtn = movie.querySelector('btn:last-of-type');
+
+//     trailerBtn.addEventListener('click', ()=>{
+//         alert('Trailer') // TODO: Cambiar por una funcionalidad real.
+//     });
+
+//     buyBtn.addEventListener('click',()=>{
+//         window.location.href = 'compra-boletos.html'; // Pagina en la cual se compran los boletos.
+//     });
+// });
+
+
+console.log(window.location.href);
